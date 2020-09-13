79402bbca4a682d15f66b1fa8ec2469a7af1cf3b
tama <ta195936ma314@gmail.com>
2020-09-13 14:44:46 +0900

    remake

--- jsprimer_todo/src/EventEmitter.js
+++ jsprimer_todo/src/EventEmitter.js
@@ -1,32 +1,50 @@
 export class EventEmitter {
-    constructor(){
+    constructor() {
+        // 登録する [イベント名, Set(リスナー関数)] を管理するMap
         this._listeners = new Map();
     }
 
+    /**
+     * 指定したイベントが実行されたときに呼び出されるリスナー関数を登録する
+     * @param {string} type イベント名
+     * @param {Function} listener イベントリスナー
+     */
     addEventListener(type, listener) {
-        if(!this._listener.has(type)){
+        // 指定したイベントに対応するSetを作成しリスナー関数を登録する
+        if (!this._listeners.has(type)) {
             this._listeners.set(type, new Set());
         }
         const listenerSet = this._listeners.get(type);
         listenerSet.add(listener);
     }
 
-    emit(type){
+    /**
+     * 指定したイベントをディスパッチする
+     * @param {string} type イベント名
+     */
+    emit(type) {
+        // 指定したイベントに対応するSetを取り出し、すべてのリスナー関数を呼び出す
         const listenerSet = this._listeners.get(type);
-        if(!listenerSet){
-            return ;
+        if (!listenerSet) {
+            return;
         }
         listenerSet.forEach(listener => {
             listener.call(this);
         });
     }
 
-    removeEventListener(type, listener){
+    /**
+     * 指定したイベントのイベントリスナーを解除する
+     * @param {string} type イベント名
+     * @param {Function} listener イベントリスナー
+     */
+    removeEventListener(type, listener) {
+        // 指定したイベントに対応するSetを取り出し、該当するリスナー関数を削除する
         const listenerSet = this._listeners.get(type);
-        if(!listenerSet){
-            return ;
+        if (!listenerSet) {
+            return;
         }
-        listenerSet.forEach(ownListener =>{
+        listenerSet.forEach(ownListener => {
             if (ownListener === listener) {
                 listenerSet.delete(listener);
             }
--- jsprimer_todo/src/model/TodoListModel.js
+++ jsprimer_todo/src/model/TodoListModel.js
@@ -1,28 +1,50 @@
-import { EventEmitter} from "../EventEmitter.js";
+import { EventEmitter } from "../EventEmitter.js";
 
-export class TodoListModel extends EventEmitter{
-    constructor(items = []){
+export class TodoListModel extends EventEmitter {
+    /**
+     * @param {TodoItemModel[]} [items] 初期アイテム一覧（デフォルトは空の配列）
+     */
+    constructor(items = []) {
         super();
         this.items = items;
     }
 
-    getTotalCount(){
+    /**
+     * TodoItemの合計個数を返す
+     * @returns {number}
+     */
+    getTotalCount() {
         return this.items.length;
     }
 
-    getTodoItems(){
+    /**
+     * 表示できるTodoItemの配列を返す
+     * @returns {TodoItemModel[]}
+     */
+    getTodoItems() {
         return this.items;
     }
 
-    onChange(listener){
+    /**
+     * TodoListの状態が更新されたときに呼び出されるリスナー関数を登録する
+     * @param {Function} listener
+     */
+    onChange(listener) {
         this.addEventListener("change", listener);
     }
 
-    emitChange(){
-        this.emitChange("change");
+    /**
+     * 状態が変更されたときに呼ぶ。登録済みのリスナー関数を呼び出す
+     */
+    emitChange() {
+        this.emit("change");
     }
 
-    addTodo(todoItem){
+    /**
+     * TodoItemを追加する
+     * @param {TodoItemModel} todoItem
+     */
+    addTodo(todoItem) {
         this.items.push(todoItem);
         this.emitChange();
     }
