319248d7878dd74c96a459864ff796f28a90b4f9
tama <ta195936ma314@gmail.com>
2020-09-21 16:04:46 +0900

    gradation

--- LifeGame/index.js
+++ LifeGame/index.js
@@ -1,18 +1,23 @@
-function draw(){
-    const ctx = document.getElementById('game_canvas').getContext('2d');
-    // Draw background
-  ctx.fillStyle = 'rgb(255,221,0)';
-  ctx.fillRect(0,0,150,37.5);
-  ctx.fillStyle = 'rgb(102,204,0)';
-  ctx.fillRect(0,37.5,150,37.5);
-  ctx.fillStyle = 'rgb(0,153,255)';
-  ctx.fillRect(0,75,150,37.5);
-  ctx.fillStyle = 'rgb(255,51,0)';
-  ctx.fillRect(0,112.5,150,37.5);
-  for (var i=0;i<10;i++){
-    ctx.fillStyle = 'rgba(255,255,255,'+(i+1)/10+')';
-    for (var j=0;j<4;j++){
-      ctx.fillRect(5+i*14,5+j*37.5,14,27.5);
-    }
+function draw() {
+    var ctx = document.getElementById('game_canvas').getContext('2d');
+  
+    // Create gradients
+    var lingrad = ctx.createLinearGradient(0,0,0,150);
+    lingrad.addColorStop(0, '#00ABEB');
+    lingrad.addColorStop(0.5, '#fff');
+    lingrad.addColorStop(0.5, '#26C000');
+    lingrad.addColorStop(1, '#fff');
+  
+    var lingrad2 = ctx.createLinearGradient(0,50,0,95);
+    lingrad2.addColorStop(0.5, '#000');
+    lingrad2.addColorStop(1, 'rgba(0,0,0,0)');
+  
+    // assign gradients to fill and stroke styles
+    ctx.fillStyle = lingrad;
+    ctx.strokeStyle = lingrad2;
+    
+    // draw shapes
+    ctx.fillRect(10,10,130,130);
+    ctx.strokeRect(50,50,50,50);
+  
   }
-}
