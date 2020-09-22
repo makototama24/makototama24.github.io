267f0f3b778f944a135cabc72fde8da979316156
tama <ta195936ma314@gmail.com>
2020-09-22 23:53:53 +0900

    think class

--- LifeGame/index.js
+++ LifeGame/index.js
@@ -1,13 +1,33 @@
-function draw() {
-    const canvas = document.getElementById('game_canvas');
-    const ctx = canvas.getContext(`2d`);
-    const row_max = 30;
-    const col_max = 30;
+const ROW_MAX = 30;
+const COL_MAX = 30;
 
-    for (let r = 0; r < row_max; r++){
-      for (let c = 0; c < col_max; c++) {
-        ctx.strokeStyle = '#DDDDDD'
-        ctx.strokeRect(r*canvas.width/row_max, c*canvas.height/col_max, canvas.width/row_max, canvas.height/col_max);
-      }
-    }
+
+function drawCanvas() {
+  const canvas = document.getElementById('game_canvas');
+  const ctx = canvas.getContext(`2d`);
+ 
+  for (let r = 0; r < ROW_MAX; r++){
+    for (let c = 0; c < COL_MAX; c++) {
+        }
   }
+}
+
+function startGame(){
+  while(1){
+    Judge();
+    Change();
+  }
+}
+
+class Cell{
+  constructor(ctx, r, c){
+    this.isAlive = false;
+    ctx.strokeStyle = '#DDDDDD'
+    ctx.strokeRect(r*canvas.width/ROW_MAX, c*canvas.height/COL_MAX, canvas.width/ROW_MAX, canvas.height/COL_MAX);
+    this.rect = rect
+  }
+
+  changeLife(){
+    this.isAlive = !this.isAlive;
+  } 
+}
