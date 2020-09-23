2dfaf10cb0c2320cd5390e1260a9d529225729bd
tama <ta195936ma314@gmail.com>
2020-09-24 00:40:26 +0900

    check pattern

--- LifeGame/canvas.js
+++ LifeGame/canvas.js
@@ -1,41 +1,51 @@
 const ROW_MAX = 30;
 const COL_MAX = 30;
+const canvas = document.getElementById('game_canvas');
 
 function drawCanvas(){
-  const canvas = document.getElementById('game_canvas');
-  const ctx = canvas.getContext(`2d`);
-
-  new Board();
+  console.log('draw')
+  const board = new Board();
 }
 
 
 class Board{
   constructor(){
+    console.log('board');
     this.map = new Array(ROW_MAX*COL_MAX);
     for(let r = 0; r < ROW_MAX; r++){
       for(let c = 0; c < COL_MAX; c++){
         const cell = new Cell(r, c);
-        this.generate(cell);
         this.map.push(cell);
       }
     }
-  }
-
-  generate(cell){
-    ctx.stroke
   }
 }
 
 class Cell{
   constructor(row, col){
+    console.log('cell')
     this.row = row;
     this.col = col;
-    this.isAlive = false;
+    if((row+col)%2 === 0){
+      this.isAlive = true;
+    }
+    else{
+      this.isAlive = false;
+    }
+    this.cell = canvas.getContext(`2d`);
+    this.render();
   }
 
-  changeLife(){
-    this.isAlive = !this.isAlive;
-  } 
+  render(){
+    if(this.isAlive){
+      this.cell.fillStyle = 'rgb(255, 190, 190)';
+      this.cell.fillRect(this.row*canvas.width/ROW_MAX, this.col*canvas.height/COL_MAX, canvas.width/ROW_MAX, canvas.height/COL_MAX);
+    }
+    else{
+      this.cell.strokeStyle = 'rgb(200, 200, 200)';
+      this.cell.strokeRect(this.row*canvas.width/ROW_MAX, this.col*canvas.height/COL_MAX, canvas.width/ROW_MAX, canvas.height/COL_MAX);
+    }
+  }
 }
 
 function startGame(){
