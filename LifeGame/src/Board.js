b46702ae273904017acf5000cb9e06db33e03b85
tama <ta195936ma314@gmail.com>
2020-09-25 03:23:51 +0900

    ,

--- LifeGame/src/Board.js
+++ LifeGame/src/Board.js
@@ -5,29 +5,27 @@ export class Board{
     constructor(){
       console.log('board')
       this.map = new Array(ROW_MAX+2, COL_MAX+2);
-      this.board = new Array(ROW_MAX, COL_MAX);
       for(let r = 0; r < (ROW_MAX+2); r++){
         for(let c = 0; c < (COL_MAX+2); c++){
           const cell = new Cell(r, c);
           this.map.push(cell);
-          if(r!=0 && c!=0 && r!=ROW_MAX+1 && c!=COL_MAX+1){
-            this.board.push(cell);
-          }
         }
       }
     }
   
     render(ctx){
-      this.board.forEach(cell => {
-        if(cell.isAlive){
-          ctx.clearRect(cell.row*Cell_width, cell.col*Cell_height, Cell_width, Cell_height);  
-          ctx.fillStyle = 'rgb(255, 190, 190)';
-          ctx.fillRect(cell.row*Cell_width, cell.col*Cell_height, Cell_width, Cell_height);  
-        }
-        else{
-          ctx.clearRect(cell.row*Cell_width, cell.col*Cell_height, Cell_width, Cell_height);  
-          ctx.strokeStyle = 'rgb(200, 200, 200)';
-          ctx.strokeRect(cell.row*Cell_width, cell.col*Cell_height, Cell_width, Cell_height);  
+      this.display.forEach(cell => {
+        if(cell.row!=0 && cell.col!=0 && cell.row!=ROW_MAX+1 && cell.col!=COL_MAX+1){
+          if(cell.isAlive){
+            ctx.clearRect(cell.row*Cell_width, cell.col*Cell_height, Cell_width, Cell_height);  
+            ctx.fillStyle = 'rgb(255, 190, 190)';
+            ctx.fillRect(cell.row*Cell_width, cell.col*Cell_height, Cell_width, Cell_height);  
+          }
+          else{
+            ctx.clearRect(cell.row*Cell_width, cell.col*Cell_height, Cell_width, Cell_height);  
+            ctx.strokeStyle = 'rgb(200, 200, 200)';
+            ctx.strokeRect(cell.row*Cell_width, cell.col*Cell_height, Cell_width, Cell_height);  
+          }
         }
       });
     }
