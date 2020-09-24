a9d0052d63ea905eb0e34b87ece44246e5b6f022
tama <ta195936ma314@gmail.com>
2020-09-24 11:12:45 +0900

    onclick function

--- LifeGame/canvas.js
+++ LifeGame/canvas.js
@@ -1,11 +1,8 @@
 const ROW_MAX = 30;
 const COL_MAX = 30;
-const canvas = document.getElementById('game_canvas');
-
-function drawCanvas(){
-  console.log('draw')
-  const board = new Board();
-}
+const Cell_width = 10;
+const Cell_height = 10;
+const drawCanvas = () => {new GameControl();};
 
 
 class Board{
@@ -19,42 +16,80 @@ class Board{
       }
     }
   }
+
+  render(ctx){
+    this.map.forEach(cell => {
+      if(cell.isAlive){
+        ctx.clearRect(cell.row*Cell_width, cell.col*Cell_height, Cell_width, Cell_height);  
+        ctx.fillStyle = 'rgb(255, 190, 190)';
+        ctx.fillRect(cell.row*Cell_width, cell.col*Cell_height, Cell_width, Cell_height);  
+      }
+      else{
+        ctx.clearRect(cell.row*Cell_width, cell.col*Cell_height, Cell_width, Cell_height);  
+        ctx.strokeStyle = 'rgb(200, 200, 200)';
+        ctx.strokeRect(cell.row*Cell_width, cell.col*Cell_height, Cell_width, Cell_height);  
+      }
+    });
+  }
 }
+
 
 class Cell{
   constructor(row, col){
     console.log('cell')
     this.row = row;
     this.col = col;
-    if((row+col)%2 === 0){
-      this.isAlive = true;
-    }
-    else{
-      this.isAlive = false;
-    }
-    this.cell = canvas.getContext(`2d`);
-    this.render();
+    this.isAlive = false;
   }
 
-  render(){
-    if(this.isAlive){
-      this.cell.fillStyle = 'rgb(255, 190, 190)';
-      this.cell.fillRect(this.row*canvas.width/ROW_MAX, this.col*canvas.height/COL_MAX, canvas.width/ROW_MAX, canvas.height/COL_MAX);
-    }
-    else{
-      this.cell.strokeStyle = 'rgb(200, 200, 200)';
-      this.cell.strokeRect(this.row*canvas.width/ROW_MAX, this.col*canvas.height/COL_MAX, canvas.width/ROW_MAX, canvas.height/COL_MAX);
-    }
+  isClick(point){
+    return (this.row*Cell_width <= point.x) && (point.x < (this.row+1)*Cell_width) && (this.col*Cell_height <= point.y) && (point.y < (this.col+1)*Cell_height);
+  }
+
+  clicked(){
+    this.isAlive = !this.isAlive;
+    console.log('clicked')
   }
 }
 
-function startGame(){
-  while(1){
-    Judge();
-    Change();
+class GameControl{
+  constructor(){
+    this.canvas = document.getElementById('game_canvas');
+    this.ctx = this.canvas.getContext(`2d`);
+    this.board = new Board();
+    this.board.render(this.ctx);
+
+    document.getElementById("start_btn").addEventListener('click', () =>this.startGame(), false);
+    document.getElementById("stop_btn").addEventListener('click', () =>this.stopGame(), false);
+    document.getElementById("reset_btn").addEventListener('click', () =>this.resetGame(), false);
+    this.canvas.addEventListener('click', e => this.clickCanvas(e), false);
+  }
+
+  startGame(){
+    console.log('start')
+  }
+
+  stopGame(){
+    console.log('stop')
+  }
+
+  resetGame(){
+    console.log('reset')
+  }
+
+  clickCanvas(e){
+    const rect = this.canvas.getBoundingClientRect();
+    const point = {
+      x: e.clientX - rect.left,
+      y: e.clientY - rect.top
+    };
+    this.board.map.forEach(cell => {
+      if(cell.isClick(point)){
+        cell.clicked();
+      }
+    });
+    this.board.render(this.ctx);
   }
 }
 
-// document.getElementById("start_btn").addEventListener('click', () =>this.startGame(), false)
-// document.getElementById("stop_btn").addEventListener('click', () =>this.startGame(), false)
-// document.getElementById("reset_btn").addEventListener('click', () =>this.startGame(), false)
+
