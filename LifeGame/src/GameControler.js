2fe3e06b1d6283e5f1cef1dbcb1560663821642c
tama <ta195936ma314@gmail.com>
2020-09-25 10:31:57 +0900

    kj

--- LifeGame/src/GameControler.js
+++ LifeGame/src/GameControler.js
@@ -43,7 +43,7 @@ export class GameControler{
   }
 
   click(e){
-    console/log(this.board.map)
+    console.log(this.board.map)
     const rect = this.canvas.getBoundingClientRect();
     const point = {
       x: e.clientX - rect.left,
