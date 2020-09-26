6a1f593da835cb1dde1176daf971b0eb51b09718
tama <ta195936ma314@gmail.com>
2020-09-26 11:14:04 +0900

    Update GameControler.js

--- LifeGame/src/GameControler.js
+++ LifeGame/src/GameControler.js
@@ -45,10 +45,10 @@ export class GameControler{
          }
        }
      }
-     console.log(`count = ${count} cell = ${cell.row}, ${cell.col} ${this.board.map.map[(cell.row+1)*(ROW_MAX+2)+(cell.col+1)].isAlive}`);
+     console.log(`count = ${count} cell = ${cell.row}, ${cell.col} ${this.board.map.map[cell.row*(ROW_MAX+2)+cell.col].isAlive}`);
  
      // 生存判定
-     if(this.board.map.map[(cell.row+1)*(ROW_MAX+2)+(cell.col+1)].isAlive){
+     if(this.board.map.map[cell.row*(ROW_MAX+2)+cell.col].isAlive){
        if(count ===2 || count ===3){
          return false;
        }
