255caede44be61162d529ae38af799eb48ad7d98
tama <ta195936ma314@gmail.com>
2020-10-11 11:31:50 +0900

    snake

--- Snake/snake.js
+++ Snake/snake.js
@@ -1,85 +1,82 @@
-window.onload = function(){
-    const ROWS = 8;
-    const COLUMNS = 8;
-    const CELLSIZE = 50;
-
-    class View{
-        static draw(map){
-            const canvas = document.getElementById("snake");
-            const ctx = canvas.getContext("2d");
-
-            map.forEach((cell, i) => {
-                const c = i%(COLUMNS+2);
-                const r = Math.floor(i/(COLUMNS+2));
-                if(c!=0 && c!=COLUMNS+1 && r!=0 && r!=ROWS+1){
-                    ctx.clearRect((c-1)*CELLSIZE, (r-1)*CELLSIZE, CELLSIZE, CELLSIZE);  
-                    if(cell.isRoute){
-                        ctx.fillStyle = "rgb(170, 220, 170)";
-                        ctx.fillRect((c-1)*CELLSIZE, (r-1)*CELLSIZE, CELLSIZE, CELLSIZE);
-                    }
-                    else{
-                        ctx.strokeStyle = "rgb(70, 70, 70)";
-                        ctx.strokeRect((c-1)*CELLSIZE, (r-1)*CELLSIZE, CELLSIZE, CELLSIZE);
-                    }
-                }
-            });
-        }
-    }
-
-    class Cell{
-        constructor(){
-            this.isRoute = false;
-        }
-
-        clear(){
-            this.isRoute = false;
-        }
-
-        change(){
-            this.isRoute = !this.isRoute;
-        }
-    }
-
-    class Board{
-        constructor(){
-            this.map = new Array((COLUMNS+2)*(ROWS+2));
-            for(let i = 0; i < (COLUMNS+2)*(ROWS+2); i++){
-                this.map[i] = new Cell();
-            }
-        }
-    }
-
+window.onload = function(){                   
     class Game{
         constructor(){
-            this.board = new Board();
-            View.draw(this.board.map);
-        
-            this.snakePos = [65, 55, 45];
-            this.add = [-1, 1, -COLUMNS-2, COLUMNS+2];
-            this.snakeDirection = 3;
-            //setIntervalのthis問題を回避
-            setInterval(this.move, 400, this);
+            this.veloX = 0;
+            this.veloY = 0;
+            this.posX = 10;
+            this.posY = 10;
+            this.gridSize = 20;
+            this.tileCount = 20;
+            this.applePosX = 15;
+            this.applePosY = 15;
+            this.trail = [];
+            this.tail = 5;
+            this.canv = document.getElementById("snake");
+            this.ctx = this.canv.getContext("2d");
+            document.addEventListener("keydown", e => this.keyPush(e));
+            setInterval(this.game, 100, this);
         }
 
-        move(game){
-            for(let i = game.snakePos.length-1; 0<=i; i--){
-                if(i!=0) game.snakePos[i] = game.snakePos[i-1];
-                else{
-                    game.snakePos[i] += game.add[game.snakeDirection];
-                    const col = game.snakePos[i] % (COLUMNS+2);
-                    const row = Math.floor(game.snakePos[i]/(COLUMNS+2));
+        game(g){
+            g.posX += g.veloX;
+            g.posY += g.veloY;
 
-                    if(col== 0) game.snakePos[i] += COLUMNS;
-                    else if(col == COLUMNS+1) game.snakePos[i] -= COLUMNS;
-                    else if(row == 0) game.snakePos[i] += (COLUMNS+2)*ROWS;
-                    else if(row == ROWS+1) game.snakePos[i] -= (COLUMNS+2)*ROWS;
+            if(g.posX < 0){
+                g.posX = g.tileCount-1;
+            }
+            else if(g.posX > g.tileCount-1){
+                g.posX = 0;
+            }
+            if(g.posY < 0){
+                g.posY = g.tileCount-1;
+            }
+            else if(g.posY > g.tileCount-1){
+                g.posY = 0;
+            }
+            g.ctx.fillStyle = "black";
+            g.ctx.fillRect(0, 0, g.canv.width, g.canv.height);
+
+            g.ctx.fillStyle = "lime";
+            for(let i = 0; i < g.trail.length; i++){
+                g.ctx.fillRect(g.trail[i].x*g.gridSize, g.trail[i].y*g.gridSize, g.gridSize-2, g.gridSize-2);
+                if(g.trail[i].x == g.posX && g.trail[i].y == g.posY){
+                    g.tail = 5;
                 }
             }
-            game.board.map.forEach(cell => cell.clear());
-            game.snakePos.forEach(pos => game.board.map[pos].change());
-            View.draw(game.board.map);
+            g.trail.push({x:g.posX, y:g.posY});
+            while(g.trail.length > g.tail){
+                g.trail.shift();
+            }
+            if(g.applePosX == g.posX && g.applePosY == g.posY){
+                g.tail++;
+                g.applePosX = Math.floor(Math.random()*g.tileCount);
+                g.applePosY = Math.floor(Math.random()*g.tileCount);
+            }
+            g.ctx.fillStyle = "red";
+            g.ctx.fillRect(g.applePosX*g.gridSize, g.applePosY*g.gridSize, g.gridSize-2, g.gridSize-2);
+        }
+
+        keyPush(evt){
+            switch(evt.keyCode){
+                case 37:
+                    this.veloX = -1;
+                    this.veloY = 0;
+                    break;
+                case 38:
+                    this.veloX = 0;
+                    this.veloY = -1;
+                    break;
+                case 39:
+                    this.veloX = 1;
+                    this.veloY = 0;
+                    break;
+                case 40:
+                    this.veloX = 0;
+                    this.veloY = 1;
+                    break;
+            }
         }
     }
 
-   new Game();
+    new Game();
 }
